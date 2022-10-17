1aa8430ba490ec5c4649c439426debeedb96d6db
likurg42 <likurg42@gmail.com>
2022-10-17 21:55:03 +0300

    solve some exercises

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.items = [];
+  }
+
+  push(elem) {
+    this.items.push(elem);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.items.length === 0) {
+      return undefined;
+    }
+    return this.items.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.items.at(-1);
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
